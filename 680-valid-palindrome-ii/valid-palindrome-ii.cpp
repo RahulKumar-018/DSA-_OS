8946a184de9c170b3dfafc78a96a8dd751906e31
Rahul_Kumar <279682917+RahulKumar-018@users.noreply.github.com>
2026-09-26 20:49:44 +0530

    Time: 3 ms (69.38%) | Memory: 22.1 MB (77.93%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
-    public:
-    bool checkPalindrome(string &s, int l, int r) {
-    while (l < r) {
-        if (s[l] != s[r]) return false;
-        l++;
-        r--;
-    }
-    return true;
-}
 public:
+    bool checkPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    
     bool validPalindrome(string s) {
-        int l = 0;
-        int r = s.size()-1;
-        while(l<r){
-            if(s[l] != s[r]) {
-                return checkPalindrome(s, l+1 ,r) || checkPalindrome(s, l, r-1);
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return checkPalindrome(s, l+1, r) || checkPalindrome(s, l, r-1);
             }
             l++;
             r--;
         }
         return true;
-
     }
 };
